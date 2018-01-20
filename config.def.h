0a4923e932957893b7e18068dc4ea18fbcf764b0
Eon S. Jeon <esjeon@hyunmu.am>
2018-01-20 22:11:45 +0900

    change the default format to clock

--- config.def.h
+++ config.def.h
@@ -1,5 +1,5 @@
 
-static char fmt[1024] = "Memory - @x[grep Avail /proc/meminfo] !";
+static char fmt[1024] = "@t";
 static int mode_xsetroot = 0;
 static int mode_loop = 0;
 static int interval = 2000; /* millisecond */
